6da3db4c90315ed76d058e7376e902e15fd9f064
Yura <104792276+Yura-111@users.noreply.github.com>
2023-01-23 22:14:15 +0200

    Add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { App } from './App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './utils/theme'
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
